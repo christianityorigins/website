7420d8b7bdb428d301e1cba2e0ed29817a13439a
ccblog2 <123avc@gmail.com>
2024-08-14 13:00:39 -0400

    Delete scripts/assets/js/target-blank.js

--- scripts/assets/js/target-blank.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const links = document.querySelectorAll('a');
-  links.forEach(link => {
-    if (link.hostname !== window.location.hostname) {
-      link.setAttribute('target', '_blank');
-      link.setAttribute('rel', 'noopener noreferrer'); // For security reasons
-    }
-  });
-});
